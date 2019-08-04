92d57cdd1ecaad9ffe9ff7f967afca129ea86884
remedialchaostheory <remedialchaostheory@users.noreply.github.com>
2019-08-03 20:52:14 -0700

    Add functionality for holding shift key to select all checkboxes in between

--- app.js
+++ app.js
@@ -1,5 +1,28 @@
 (function () {
   'use strict';
 
+  const checkboxes = document.querySelectorAll('.inbox input[type="checkbox"');
+
+  let lastChecked;
+
+  function handleCheck(e) {
+    // Check if shift key is down
+    let inBetween = false;
+
+    if (e.shiftKey && this.checked) {
+      checkboxes.forEach(checkbox => {
+        if (checkbox === this || checkbox === lastChecked) {
+          inBetween = !inBetween;
+        }
+
+        if (inBetween) {
+          checkbox.checked = true;
+        }
+      });
+    }
+
+    lastChecked = this;
+  }
+  checkboxes.forEach(checkbox => checkbox.addEventListener('click', handleCheck));
 
 })();
